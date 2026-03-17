c9bc8c1dcea6d405f35835dc9dd73bfe7c8038c3
ksvaza <ksvaza@gmail.com>
2026-03-17 10:30:36 +0200

    Implementēts viss vajadzīgais logošanai ar logrus.
    Vajadzētu iestatīt logrus izvadu kā failu, taču to vēlāk.

--- cmd/ees-link/main.go
+++ cmd/ees-link/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/ksvaza/ees-link/logeris"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("Sveika, pasaule!")
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.WithError(errors.New(fmt.Sprintf("%v", r))).Error("Panic")
+		}
+	}()
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "02.01.2006 15:04:05.000",
+		FullTimestamp:   true,
+		//		DisableColors:   true,
+		DisableQuote: true,
+	})
+	logrus.SetOutput(&logeris.LogWriter{})
+
+	logrus.AddHook(&logeris.StacktraceHook{})
+
+	logrus.SetLevel(logrus.InfoLevel)
+
+	for i := 0; i < 5; i++ {
+		logrus.Tracef("Trace %d", i)
+		logrus.Debugf("Debug %d", i)
+		logrus.Infof("Info %d", i)
+		logrus.Warnf("Warn %d", i)
+		logrus.Errorf("Error %d", i)
+	}
+
+	for {
+
+	}
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ksvaza/ees-link
+
+go 1.25.4
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.4
+)
+
+require golang.org/x/sys v0.13.0 // indirect

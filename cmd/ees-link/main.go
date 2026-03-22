7b0e3d6c66cb736091311f5871268b0ac93651e1
ksvaza <ksvaza@gmail.com>
2026-03-22 14:50:56 +0200

    Tagad logrus izvads ir fails log.txt.
    log.txt ierakstīts .gitignore.

--- cmd/ees-link/main.go
+++ cmd/ees-link/main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ksvaza/ees-link/logeris"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
+
+func setupLogger(logFile string) (f *os.File, err error) {
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "02.01.2006 15:04:05.000",
+		FullTimestamp:   true,
+		//		DisableColors:   true,
+		DisableQuote: true,
+	})
+
+	f, err = os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+
+	// Output to stdout instead of the default stderr
+	logrus.SetOutput(f)
+
+	//logrus.SetOutput(&logeris.LogWriter{})
+
+	logrus.AddHook(&logeris.StacktraceHook{})
+
+	logrus.SetLevel(logrus.InfoLevel)
+
+	return
+}
 
 func main() {
 	defer func() {
@@ -15,17 +39,13 @@ func main() {
 		}
 	}()
 
-	logrus.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "02.01.2006 15:04:05.000",
-		FullTimestamp:   true,
-		//		DisableColors:   true,
-		DisableQuote: true,
-	})
-	logrus.SetOutput(&logeris.LogWriter{})
-
-	logrus.AddHook(&logeris.StacktraceHook{})
-
-	logrus.SetLevel(logrus.InfoLevel)
+	logfile := "./log.txt"
+	f, err := setupLogger(logfile)
+	if err != nil {
+		fmt.Println("Failed to create logfile" + logfile)
+		panic(err)
+	}
+	defer f.Close()
 
 	for i := 0; i < 5; i++ {
 		logrus.Tracef("Trace %d", i)

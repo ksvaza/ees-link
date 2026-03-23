7cf796b7cee23132168573e7cb229144da7c89fc
ksvaza <ksvaza@gmail.com>
2026-03-23 14:29:33 +0200

    Testēšana ---------
    Uzraktīts vienību testu piemērs t_test.go.
    
    Papildināts būvskripts, lai tiktu palaisti visi testi un būvēša varētu turpināties tikai ar visiem testiem veiksmīgi pabeigtiem.

---
+++ t_test.go
@@ -0,0 +1,12 @@
+package t_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// testu tests
+func TestSomething(t *testing.T) {
+	require.True(t, true, "Vienmēr ir jābūt patiesam")
+}

11604ab7562077bfd9df3a7a0413043abb57cec5
IngloriousBasterd593 <adriansdacko@gmail.com>
2026-03-24 23:14:53 +0200

    Tika implementēta tabulu izveidošana, datu seriāla ielikšana un nolasīšana no tabulām pēc mainīgā.

--- cmd/ees-link/main.go
+++ cmd/ees-link/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/ksvaza/ees-link/db"
 	"github.com/ksvaza/ees-link/envreader"
@@ -42,9 +43,76 @@ func main() {
 		logrus.Errorf("Error %d", i)
 	}
 
-	fmt.Print(db.Kkas())
+	// sūdu aispildīšana
+
+	// fmt.Printf("karukskigi\n")
+	/*
+		var events []*db.Event
+		numEvents := 1000 // number of events to generate
+
+		for i := 0; i < numEvents; i++ {
+			e := &db.Event{
+				InstanceID: rand.Intn(30) + 1, // random instance 1–30
+				Name:       fmt.Sprintf("metric_%d", rand.Intn(1000)),
+				Value:      rand.Float64() * 100.0, // random float 0–100
+				Timestamp:  time.Now().Add(time.Duration(rand.Intn(1000)) * time.Millisecond),
+			}
+			events = append(events, e)
+		}
+	*/
+
+	events := []*db.Event{
+		{
+			InstanceID: 1,
+			Name:       "temp",
+			Value:      23.5,
+			Timestamp:  time.Now(),
+		},
+		{
+			InstanceID: 2,
+			Name:       "humidity",
+			Value:      65.0,
+			Timestamp:  time.Now(),
+		},
+	}
+
+	db.Init("postgres://postgres:postgres@localhost:8086/demo?sslmode=disable")
+	err = db.InsertEventsBatchToTable(events, "dalibnieki")
+	if err != nil {
+		logrus.WithError(err).Error("Failed to insert events batch")
+		return
+	}
+
+	dabut, err := db.GetAllEventsByName("temp")
+	if err != nil {
+		logrus.WithError(err).Error("Failed to get all events")
+		return
+	}
+	for _, e := range dabut {
+		fmt.Printf("Event: ID=%d, InstanceID=%d, Name=%s, Value=%.2f, Timestamp=%s\n",
+			e.ID, e.InstanceID, e.Name, e.Value, e.Timestamp.Format(time.RFC3339))
+	}
+
+	/*
+
+		err1 := db.Init("postgres://postgres:postgres@localhost:8086/demo?sslmode=disable")
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		defer db.Close()
+
+		e := db.Event{InstanceID: 1, Name: "temp", Value: 23.5, Timestamp: time.Now()}
+		err = db.InsertEvent(&e)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Inserted event: %+v\n", e)
+
+	*/
 
 	for {
+		//fmt.Printf("nig")
+		//time.Sleep(2000) // 2000ms
 
 	}
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,20 @@ module github.com/ksvaza/ees-link
 go 1.25.4
 
 require (
+	github.com/jackc/pgx/v5 v5.9.1
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.4
 	github.com/stretchr/testify v1.11.1
+	golang.org/x/exp v0.0.0-20260312153236-7ab1446f8b90
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
@@ -21,12 +26,12 @@ require (
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
-	github.com/lib/pq v1.12.0
 	github.com/spf13/viper v1.21.0
 	golang.org/x/sys v0.29.0 // indirect
 )

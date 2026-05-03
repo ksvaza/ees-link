69ea7e7c75682d5858fb953a4fb2b11b0cdf04d7
ksvaza <ksvaza@gmail.com>
2026-05-04 00:23:19 +0300

    Tīrīšana

--- db/db.go
+++ db/db.go
@@ -458,10 +458,10 @@ func (DB *RealDB) GetAccounts(ctx context.Context) ([]models.Account, error) {
 	return accounts, nil
 }
 
-func (DB *RealDB) GetAccountsByID(ctx context.Context, id string) ([]models.Account, error) {
-	rows, err := Pool.Query(ctx, AccountReadRequestByID, id)
+func (DB *RealDB) GetAccountsByKey(ctx context.Context, key string) ([]models.Account, error) {
+	rows, err := Pool.Query(ctx, AccountReadRequestByKey, key)
 	if err != nil {
-		logrus.WithError(err).Error("Failed to query accounts table by ID")
+		logrus.WithError(err).Error("Failed to query accounts table by key")
 		return nil, err
 	}
 	defer rows.Close()
@@ -489,7 +489,7 @@ func (DB *RealDB) GetAccountsByID(ctx context.Context, id string) ([]models.Acco
 	}
 
 	if err = rows.Err(); err != nil {
-		logrus.WithError(err).Error("Failed iterating account rows by ID")
+		logrus.WithError(err).Error("Failed iterating account rows by key")
 		return nil, err
 	}
 
